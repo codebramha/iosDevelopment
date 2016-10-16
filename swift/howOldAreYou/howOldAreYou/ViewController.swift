e2017a488ca2b66c178c1435cc7c61be02cd8433
codebramha <codebramha@gmail.com>
2016-10-16 16:36:41 -0700

    Fixing the file issue

---
+++ swift/howOldAreYou/howOldAreYou/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  howOldAreYou
+//
+//  Created by harikanth on 10/11/16.
+//  Copyright © 2016 Code Bramha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
