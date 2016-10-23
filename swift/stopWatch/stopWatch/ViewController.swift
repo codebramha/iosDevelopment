edf75dc887dc0e66067851bf3192d94ee1067679
codebramha <codebramha@gmail.com>
2016-10-22 18:12:19 -0700

    stopWatch app

---
+++ swift/stopWatch/stopWatch/ViewController.swift
@@ -0,0 +1,123 @@
+//
+//  ViewController.swift
+//  navigationBar
+//
+//  Created by harikanth on 10/17/16.
+//  Copyright © 2016 Code Bramha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var laps: [String] = []
+    var timer = Timer()
+    var minutes: Int = 0
+    var seconds: Int = 0
+    var fractions: Int = 0
+    
+    var startStopWatch: Bool = true
+    var addLap: Bool = false
+    var stopWatchString: String = ""
+    
+    @IBOutlet var stopWatchLabel: UILabel!
+    
+    @IBOutlet var lapsTableView: UITableView!
+    @IBOutlet var startStopButton: UIButton!
+    @IBOutlet var lapResetButton: UIButton!
+    
+    @IBAction func startStop(_ sender: AnyObject) {
+        
+        if startStopWatch == true {
+            timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateStopWatch), userInfo: nil, repeats: true)
+            startStopWatch = false
+            startStopButton.setImage(#imageLiteral(resourceName: "pause.png"), for: UIControlState.normal)
+            lapResetButton.setImage(#imageLiteral(resourceName: "lap.png"), for: UIControlState.normal)
+            addLap = true
+        } else {
+            
+            timer.invalidate()
+            startStopWatch = true
+            startStopButton.setImage(#imageLiteral(resourceName: "play-button.png"), for: UIControlState.normal)
+            lapResetButton.setImage(#imageLiteral(resourceName: "reset1.png"), for: UIControlState.normal)
+            addLap = false
+            
+        }
+        
+        
+    }
+    
+    
+    @IBAction func lapReset(_ sender: AnyObject) {
+        
+        if addLap == true {
+            
+            laps.insert("\(stopWatchString)", at: 0)
+            lapsTableView.reloadData()
+            
+        } else {
+            addLap = false
+            lapResetButton.setImage(#imageLiteral(resourceName: "lap.png"), for: UIControlState.normal)
+            fractions = 0
+            seconds = 0
+            minutes = 0
+            
+            stopWatchString = "00:00.00"
+            stopWatchLabel.text = stopWatchString
+        }
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stopWatchLabel.text = "00:00.00"
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func updateStopWatch(){
+        fractions += 1
+        if fractions == 100 {
+            seconds += 1
+            
+            fractions = 0
+        }
+        if seconds == 60 {
+            minutes += 1
+            seconds = 0
+        }
+        
+        let fractionsString = fractions > 9 ? "\(fractions)" : "0\(fractions)"
+        
+        let secondsString = seconds > 9 ? "\(seconds)" : "0\(seconds)"
+        let minutesString = minutes > 9 ? "\(minutes)" : "0\(minutes)"
+        
+        stopWatchString = "\(minutesString):\(secondsString).\(fractionsString)"
+        stopWatchLabel.text = stopWatchString
+    }
+    
+    
+    // Table view Methods
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.value1, reuseIdentifier: "Cell")
+        cell.backgroundColor = self.view.backgroundColor
+        cell.textLabel?.text = "Lap \(laps.count - indexPath.row)"
+        cell.detailTextLabel?.text = laps[indexPath.row]
+        return cell
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return laps.count
+    }
+    
+}
+
